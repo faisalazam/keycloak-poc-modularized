a1d09926271204640b12443d5b7c7fb3c978b455
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-11-19 18:04:20 +1100

    Fixed escaping issues in the script.

--- .github/workflows/scripts/check_health.sh
+++ .github/workflows/scripts/check_health.sh
@@ -6,7 +6,7 @@ CONTAINER_NAME="keycloak"
 if ! timeout 100s sh -c "
   until docker exec $CONTAINER_NAME sh -c 'exec 3<>/dev/tcp/127.0.0.1/9000 &&
           echo -e \"GET /health/ready HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n\" >&3 &&
-          grep -q '\"status\": \"UP\"' <&3'; do
+          grep -q \"\\\"status\\\": \\\"UP\\\"\" <&3'; do
     echo 'Waiting for service to start...'
     sleep 5
   done

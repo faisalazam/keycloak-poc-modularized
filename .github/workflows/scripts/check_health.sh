b14db64e17348d08f72a7046dafeffd55ec83e26
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-11-19 17:43:09 +1100

    Extracted container name to a variable.

--- .github/workflows/scripts/check_health.sh
+++ .github/workflows/scripts/check_health.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+
+CONTAINER_NAME="keycloak"
+
 # Set the timeout duration to 100 seconds for the health check
-if ! timeout 100s sh -c '
-  until docker exec <container_name> sh -c "
-    exec 3<>/dev/tcp/127.0.0.1/9000 &&
-    echo -e \"GET /health/ready HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n\" >&3 &&
-    grep -q \"\\\"status\\\": \\\"UP\\\"\" <&3
-  "; do
-    echo "Waiting for service to start inside the container..."
+if ! timeout 100s sh -c "
+  until docker exec $CONTAINER_NAME sh -c 'exec 3<>/dev/tcp/127.0.0.1/9000 &&
+          echo -e \"GET /health/ready HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n\" >&3 &&
+          grep -q '\"status\": \"UP\"' <&3'; do
+    echo 'Waiting for service to start...'
     sleep 5
   done
-'; then
+"; then
     echo "Service health check timed out."
     exit 1
 fi

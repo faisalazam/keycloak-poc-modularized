0197b5cd2faf379d39e4882078231b1f1d0acbba
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-11-25 12:16:21 +1100

    Added some print statements.

--- apache-server/startup_script.sh
+++ apache-server/startup_script.sh
@@ -2,9 +2,12 @@
 
 if [ "$SETUP_KEYCLOAK_PROXY" = "true" ]; then
   echo "Define ENABLE_KEYCLOAK_PROXY" >> /usr/local/apache2/conf/extra/enable-keycloak.conf
+  echo "SUCCESS: ENABLE_KEYCLOAK_PROXY defined in enable-keycloak.conf"
 else
   echo "" > /usr/local/apache2/conf/extra/enable-keycloak.conf
+  echo "SUCCESS: Cleared enable-keycloak.conf as SETUP_KEYCLOAK_PROXY is not true"
 fi
 
 # Start Apache
-httpd-foreground
+echo "Starting Apache..."
+httpd-foreground && echo "SUCCESS: Apache started successfully" || echo "FAILURE: Apache failed to start"

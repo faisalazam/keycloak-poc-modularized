05a79031b248cf77a7dba35ed5f13dd463b95c58
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-11-27 17:32:59 +1100

    Added some comments.

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# This is only for local/CI for running tests. Use ./start.sh for non-dev use.
+
 # Source the helper script to access the cleanup_and_start function
 . ./startup_helper.sh
 

9f171460b7aa9048e2ec9a4a0d4d519b8847015f
Muhammad Faisal <faisalazam_1616@hotmail.com>
2024-11-28 03:42:07 +1100

    Used start.sh script from ci.sh

--- ci.sh
+++ ci.sh
@@ -2,8 +2,4 @@
 
 # This is only for local/CI for running tests. Use ./start.sh for non-dev use.
 
-# Source the helper script to access the cleanup_and_start function
-. ./startup_helper.sh
-
-# Call the function to clean up and start the services
-EXPOSE_KEYCLOAK_TO_HOST_ONLY=true cleanup_and_start
+EXPOSE_KEYCLOAK_TO_HOST_ONLY=true ./start.sh
